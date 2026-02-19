a6b6c56489601904d10e3a25fa8f7cfbbd3a243b
Dani <damen02.info@gmail.com>
2026-02-19 16:52:54 +0100

    Propiertes configurado

---
+++ src/main/java/com/mdz/angular_spring/AngularSpringApplication.java
@@ -0,0 +1,12 @@
+package com.mdz.angular_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularSpringApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AngularSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdz/angular_spring/AngularSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdz.angular_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
